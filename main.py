88b688859d7c90a471aa0edad1d9eb5de704003e
broGideon <144320082+broGideon@users.noreply.github.com>
2023-10-30 23:49:27 +0300

    main.py

--- main.py
+++ main.py
@@ -79,15 +79,6 @@ def end(end):
             writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=';')
             writer.writeheader()
             writer.writerow(dict)
-
-    for csvfile in glob.glob(os.path.join('.', '*.csv')):
-        workbook = Workbook(csvfile[:-5] + '.xlsx')
-    worksheet = workbook.add_worksheet()
-    with open(csvfile, 'rt', encoding='utf8') as f:
-        reader = csv.reader(f)
-        for r, row in enumerate(reader):
-            for c, col in enumerate(row):
-                worksheet.write(r, c, col)
     exit()
 
 
